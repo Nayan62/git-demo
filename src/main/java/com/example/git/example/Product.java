5f2804ce98dc61ca0cead6002df8f7802f3cb905
Nayan62 <Nayanjyotisahu123@gmail.com>
2025-02-15 16:54:39 +0530

    added

---
+++ src/main/java/com/example/git/example/Product.java
@@ -0,0 +1,22 @@
+package com.example.git.example;
+
+public class Product {
+    private int productid;
+    private String productname;
+
+    public int getProductid() {
+        return productid;
+    }
+
+    public void setProductid(int productid) {
+        this.productid = productid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+}
